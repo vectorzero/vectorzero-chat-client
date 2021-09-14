d87b79e8010b1a62f30428e82c086777795489d1
林贤亮-01384950 <xianlianglin@sfmail.sf-express.com>
2021-09-14 18:17:20 +0800

    ✨ feat(服务): 加入mock服务

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        cookieDomainRewrite: '',
+        secure: false
+      }
+    }
+  },
   plugins: [reactRefresh()]
 })
